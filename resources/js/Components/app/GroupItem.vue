31062d9517d7964b1b9fa2d787f71bd8db6d121c
sangla3 <lavansang1264@gmail.com>
2024-06-04 18:18:08 +0700

    update

--- resources/js/Components/app/GroupItem.vue
+++ resources/js/Components/app/GroupItem.vue
@@ -9,7 +9,7 @@ defineProps({
 <template>
     <div class="mb-3 cursor-pointer hover:bg-gray-100">
         <Link :href="route('group.profile', group.slug)" class="flex items-start gap-1 py-2 px-2">
-            <img :src="group.thumbnail_url" class="w-[40px] rounded-full">
+            <img :src="group.thumbnail_url" class="w-[40px] h-[40px] rounded-full">
             <div class="flex-1">
                 <div class="flex justify-between">
                     <h3 class="font-sans text-xl">{{ group.name }}</h3>
